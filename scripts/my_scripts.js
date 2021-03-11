93809a97de8fe54ee9ecbd2244fc5d84fa79f421
Raquel Alves Pereira <79079796+raqxo@users.noreply.github.com>
2021-03-11 20:22:52 +0100

    klok af
    klok is af, veranderd op kleur

--- scripts/my_scripts.js
+++ scripts/my_scripts.js
@@ -10,3 +10,36 @@ setInterval(() => {
 	mn.style.transform = `rotateZ(${mm}deg)`;
 	sc.style.transform = `rotateZ(${ss}deg)`;
 })
+
+function updateBackground() { 
+ 	var dayz = new Date();
+  	var hh = dayz.getHours();
+  	var sss = dayz.getSeconds();
+
+	if (hh >= 6 && hh <= 10) {
+    document.body.style.backgroundColor='#EF810E';
+	} else if (hh >= 11 && hh <= 20) {
+    document.body.style.backgroundColor='#053752';
+	} else {
+    document.body.style.backgroundColor='#001A26';
+	}	
+} 
+
+
+
+// function updateBackground() { 
+//  	var dayz = new Date();
+//   	var hh = dayz.getHours();
+//   	var sss = dayz.getSeconds();
+
+// 	if (sss >= 6 && sss <= 10) {
+//     document.body.style.backgroundColor='#EF810E';
+// 	} else if (sss >= 11 && sss <= 20) {
+//     document.body.style.backgroundColor='#053752';
+// 	} else {
+//     document.body.style.backgroundColor='#001A26';
+// 	}	
+// }
+
+setInterval(updateBackground, 500);
+updateBackground();
